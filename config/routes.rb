5745529421ec7ffa77391a7564db4b5e25c80ec9
Christian Bondzie <christian.zap@hotmail.fr>
2023-02-22 20:42:03 +0100

    Exercice 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :restaurants, only: [:index, :create, :show, :new] do
+    resources :reviews, only: [:new, :create]
+  end
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
